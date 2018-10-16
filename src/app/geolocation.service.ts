23a3bd6174255f10338e8d7ebcbd60de3fa0df73
root <root@localhost.localdomain>
2018-10-16 06:42:46 +0000

    Geolocation service added

---
+++ src/app/geolocation.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeolocationService {
+
+  constructor() { }
+
+  requestLocation(callback) {
+    // W3c Geolocation API
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        callback(position.coords);
+      },
+      error => {
+        callback(null);
+      }
+    )
+  }
+
+  getMapLink(location: PlaceLocation) {
+    let query = "";
+    if(location.latitude) {
+      query = location.latitude + "," + location.longitude;
+    } else {
+      query = `${location.address}, ${location.city}`;
+    }
+    if(/iPad|iPhone|iPad/.test(navigator.userAgent)) {
+      return `https://maps.apple.com/?q=${query}`;
+    } else {
+      return `https://maps.google.com/?q=${query}`;
+    }
+    // Universal Link
+    // <a href="https://maps.google.com/?q=Eiffel+Tower">
+    // <a href="https://maps.google.com/?q=19.0211754,73.0916062">
+    // <a href="https://maps.apple.com/?q=19.0211754,73.0916062">
+  }
+}
