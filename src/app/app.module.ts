d64f46487ebdc63b3d7b18de2cecde4ffbe6653d
root <root@localhost.localdomain>
2018-10-18 07:22:51 +0000

    Added routes for components Coffee and List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,24 @@ import { DataService } from './data.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule, MatToolbarModule, MatCardModule, MatSlideToggleModule } from "@angular/material";
 import 'hammerjs';
+import { ListComponent } from './list/list.component';
+import { CoffeeComponent } from './coffee/coffee.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes : Routes = [
+  { path: '', component: ListComponent },
+  { path: 'coffee', component: CoffeeComponent },
+  { path: 'coffee/:id', component: CoffeeComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CoffeeComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule, BrowserAnimationsModule, MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule, MatToolbarModule, MatCardModule, MatSlideToggleModule
   ],
   providers: [GeolocationService, DataService],
